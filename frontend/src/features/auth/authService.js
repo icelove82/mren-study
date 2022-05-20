f7da6a9557478705cf8d13599c0fd554d66d1118
YUN <yunmyeonghun@gmail.com>
2022-05-20 18:40:59 +0900

    17 finish login

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // register api
-const API_URL = '/api/users';
+const API_URL = '/api/users/';
 
 // register user
 const register = async (userData) => {
@@ -14,13 +14,25 @@ const register = async (userData) => {
   return response.data;
 };
 
+// login
+const login = async (userData) => {
+  const response = await axios.post(API_URL + 'login', userData);
+
+  if (response.data) {
+    localStorage.setItem('user', JSON.stringify(response.data));
+  }
+
+  return response.data;
+};
+
 // logout user
-const logout = () => {
+const logout = async () => {
   localStorage.removeItem('user');
 };
 
 const authService = {
   register,
+  login,
   logout,
 };
 
