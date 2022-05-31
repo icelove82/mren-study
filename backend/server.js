b92f13a397a23f048a71232855b94f89dae38858
YUN <yunmyeonghun@gmail.com>
2022-05-31 11:10:20 +0900

    21 production deploy

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const colors = require('colors');
 const dotenv = require('dotenv').config();
@@ -19,6 +20,17 @@ app.use('/api/goals', require('./routes/goalRoute'));
 
 // users route sets
 app.use('/api/users', require('./routes/userRoute'));
+
+// serve frontend
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+    )
+  );
+}
 
 // middleware - for custome error handler
 app.use(errorHandler);
